8a54a3948e6c3cb478c8f05c289d7dcd4ac68e21
mightyprinces <happyrabbit62@gmail.com>
2025-11-11 03:55:02 +0800

    feat: solved binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,43 +1,137 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.tree = null;
+  }
+
   root() {
     return this.tree;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  addNode(node) {
+    if (!this.tree) {
+      this.tree = node;
+    } else if (!this.has(node.data)) {
+      let current = this.tree;
+
+      while (true) {
+        if (current.data > node.data) {
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = node;
+            break;
+          }
+        } else {
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = node;
+            break;
+          }
+        }
+      }
+    }
   }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    const node = new Node(data);
+    this.addNode(node);
   }
 
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  findWithParent(data) {
+    if (!this.tree) {
+      return [null, null];
+    } else {
+      let current = this.tree;
+      let parent = null;
+
+      while (true) {
+        if (current.data === data) {
+          return [current, parent];
+        }
+        if (current.data > data) {
+          if (current.left) {
+            parent = current;
+            current = current.left;
+          } else {
+            return [null, null];
+          }
+        } else {
+          if (current.right) {
+            parent = current;
+            current = current.right;
+          } else {
+            return [null, null];
+          }
+        }
+      }
+    }
   }
 
-  remove(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    return this.findWithParent(data)[0];
+  }
+
+  has(data) {
+    return !!this.find(data);
+  }
+
+  remove(data) {
+    const [node, parent] = this.findWithParent(data);
+
+    if (node) {
+      if (parent) {
+        if (parent.left === node) {
+          parent.left = null;
+        }
+        if (parent.right === node) {
+          parent.right = null;
+        }
+      } else {
+        this.tree = null;
+      }
+      if (node.left) {
+        this.addNode(node.left);
+      }
+      if (node.right) {
+        this.addNode(node.right);
+      }
+    }
   }
 
   min() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.tree) {
+      return null;
+    } else {
+      let current = this.tree;
+
+      while (current.left) {
+        current = current.left;
+      }
+
+      return current.data;
+    }
   }
 
   max() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.tree) {
+      return null;
+    } else {
+      let current = this.tree;
+
+      while (current.right) {
+        current = current.right;
+      }
+
+      return current.data;
+    }
   }
 }
 
